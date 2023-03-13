9d99522e4eb87bbcb4cea6b14b691a8bc84bcc10
gexiaoxiao7 <1004083966@qq.com>
2023-03-13 23:10:35 +0800

    update

--- oj/views.py
+++ oj/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, "index.html")
+
+def login(request):
+    return render(request, "log_in.html")
+
+def register(request):
+    return HttpResponse('注册页面！')
+
+

11d3e0714e9bda67a12045ee7fad5cc92d2ce190
gexiaoxiao7 <1004083966@qq.com>
2023-03-15 22:42:09 +0800

    update

--- oj/views.py
+++ oj/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
 # Create your views here.
+from oj import models
 
 
 def index(request):
@@ -15,6 +15,25 @@ def login(request):
     return render(request, "log_in.html")
 
 def register(request):
+    successMessage = '注册成功'
+    errMessage = '注册失败'
+    if request.method == 'POST':
+        userAccount = request.POST.get('userAccount')
+        password = request.POST.get('password')
+        checkPassword = request.POST.get('checkPassword')
+
+        # 校验密码
+        if password == checkPassword:
+            #TODO:md5加密
+            #TODO:默认用户昵称做一下唯一标识（比如用户89231）
+            models.Suiber.objects.create(
+                account= userAccount ,
+                password= password,
+                nickName= '默认用户昵称'
+            )
+        else:
+            return render(request , 'register.html' , {'errMessage': errMessage})
+        return render(request,'log_in.html', {'successMessage': successMessage})
     return render(request, "register.html")
 
 

969ae9dd145a825e64293f19b30681948c4d430d
Stan1ey <1760711322@qq.com>
2023-09-21 20:54:19 +0800

    update

--- suibeOJ/settings.py
+++ suibeOJ/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "suibeOJ",
         "USER": "root",
-        "PASSWORD": "s2003s01f20",
+        "PASSWORD": "123456",
         "HOST": "127.0.0.1",
         "PORT": 3306
     }
